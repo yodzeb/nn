7c5129d18f5223d2a8896ae657071fcdac1de314
yodzeb <romspam@wiro.fr>
2017-12-09 14:25:45 +0100

    improvements

--- http_nn.py
+++ http_nn.py
@@ -9,55 +9,65 @@ import numpy
 word_size = 100
 max_words = 128
 
-# NN variables, same for everyone
-W = tf.Variable(tf.random_normal([6656,5]), trainable=True)
-b = tf.Variable(tf.random_normal([5]), trainable=True)
+tf.logging.set_verbosity(tf.logging.INFO)
 
-filter_1st_layer = tf.Variable(tf.random_normal([5,5,1,1]), trainable=True)
-bias_1st =         tf.Variable(tf.zeros([max_words]))
-
-filter_2nd_layer = tf.Variable(tf.random_normal([3,3,64,64]), trainable=True)
-bias_2nd =         tf.Variable(tf.zeros([max_words/2]))
-
-def get_nn(input, labels, mode):
+def get_nn(features, labels, mode):
     #keep_prob=0.8
-    input= tf.reshape(input, [-1, 128, 100, 1])
-    print input
-
+    input= tf.reshape(features["train/words"], [-1, 128, 100, 1])
+    
     one = tf.layers.conv2d (inputs=input,
-                            filters=64,
+                            filters=32,
                             kernel_size=[ 32, 4 ],
                             padding="same",
                             activation=tf.nn.relu)
     one_mp = tf.layers.max_pooling2d(inputs=one, pool_size=[2,2],strides=2)
-    print one_mp
     two = tf.layers.conv2d (inputs=one_mp,
-                            filters=128,
+                            filters=64,
                             kernel_size=[32,4 ],
                             padding="same",
                             activation=tf.nn.relu)
     two_mp = tf.layers.max_pooling2d(inputs=two, pool_size=[2,2], strides=2)
-    print two_mp
     # Dense Layer
-    pool2_flat = tf.reshape(two_mp, [-1, 32*25*128])
-    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+    pool2_flat = tf.reshape(two_mp, [-1, 32*25*64])
+    dense = tf.layers.dense(inputs=pool2_flat, units=512, activation=tf.nn.relu)
     dropout = tf.layers.dropout(
         inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
-    print dropout
-
+    
     # Logits Layer
     y__ = tf.layers.dense(inputs=dropout, units=5)
     y = tf.nn.softmax(y__)
     
     print y
-    
-    #y             = tf.nn.softmax(tf.add(tf.matmul(resh, W), b)) #y = tf.nn.relu(tf.matmul(x,W) + b)
-    cross_entropy = tf.reduce_mean(-tf.reduce_sum(labels * tf.log(y), reduction_indices=[1]))
-    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(labels,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    return y, cross_entropy, accuracy
+    predictions = tf.reshape(y, [-1])
+    loss = tf.losses.mean_squared_error(labels, predictions)
 
-def parse_record ( filename, batch_s, shuffle ):
+    eval_metric_ops = {
+        "rmse": tf.metrics.root_mean_squared_error(tf.cast(labels, tf.float32), predictions),
+        "labels": labels,
+        "output": predictions
+    }
+    
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.03)
+    train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+    
+    predictions_dict = {
+        "result": predictions,
+        }
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        loss=loss,
+        train_op=train_op,
+        eval_metric_ops=eval_metric_ops,
+        predictions=predictions)
+    #return y, cross_entropy, accuracy
+
+def train_fn():
+    return parse_record("http_train.tfrec", 100)
+
+def eval_fn():
+    return parse_record("http_valid.tfrec", 10)
+    
+def parse_record ( filename, batch_s ):
     filename_queue = tf.train.string_input_producer([filename])
     reader     = tf.TFRecordReader()
     key, value = reader.read(filename_queue)
@@ -68,52 +78,70 @@ def parse_record ( filename, batch_s, shuffle ):
             'train/words': tf.FixedLenFeature([word_size * max_words], tf.float32)
         }
     )
-    label = tf.cast(features['train/label'], tf.float32)
-    line  = tf.reshape(tf.cast(features['train/words'], tf.float32), [max_words, word_size])
-    if shuffle:
-        return tf.train.shuffle_batch([label, line], batch_size=batch_s, capacity=4, min_after_dequeue=1)
-    else:
-        return tf.train.batch([label, line], batch_size=batch_s, capacity=10 )
+    #return features
+    label = tf.cast(features['train/label'], tf.float64)
+    line  = tf.reshape(tf.cast(features['train/words'], tf.float64), [max_words, word_size])
+    return features, label
+
+nn = tf.estimator.Estimator(model_fn=get_nn)
+
+for i in range(1,1):
+    nn.train(input_fn=train_fn, steps=100)
+
+print "Now evaluating"
+
+ev = nn.evaluate(input_fn=eval_fn, steps=10)
+print("Loss: %s" % ev["loss"])
+print("Root Mean Squared Error: %s" % ev["rmse"])
+print("labels"+ ev["labels"]);
+print("preds"+  ev["output"]);
 
 
-filename_queue_test = tf.train.string_input_producer(["simple_real.tfrecords2"])
 
-label,      line      = parse_record("http_train.tfrec", 10,  False)
-label_test, line_test = parse_record("http_valid.tfrec", 10, True)
+# label,      line      = 
+# label_test, line_test = 
 
-y, ce, acc       = get_nn(line,      label,      tf.estimator.ModeKeys.TRAIN)
-y_t, ce_t, acc_t = get_nn(line_test, label_test, tf.estimator.ModeKeys.EVAL)
 
-train_step = tf.train.AdamOptimizer(0.003).minimize(ce)
-model = tf.global_variables_initializer()
-coord = tf.train.Coordinator()
-NUM_THREADS=2
-saver = tf.train.Saver()
 
-with tf.Session(config=tf.ConfigProto(intra_op_parallelism_threads=NUM_THREADS)) as session:
-    #saver.restore(session, "/tmp/model.ckpt")
-    #print("Model restored.")
-    #print W.eval()
-    #sys.exit(0)
-    session.run(model)
-    threads=tf.train.start_queue_runners(sess=session, coord=coord)
+#y, ce, acc       = get_nn(line,      label,      tf.estimator.ModeKeys.TRAIN)
+#y_t, ce_t, acc_t = get_nn(line_test, label_test, tf.estimator.ModeKeys.EVAL)
+
+
+
+
+
+
+
+# train_step = tf.train.AdamOptimizer(0.003).minimize(ce)
+# model = tf.global_variables_initializer()
+# coord = tf.train.Coordinator()
+# NUM_THREADS=2
+# saver = tf.train.Saver()
+
+# with tf.Session(config=tf.ConfigProto(intra_op_parallelism_threads=NUM_THREADS)) as session:
+#     #saver.restore(session, "/tmp/model.ckpt")
+#     #print("Model restored.")
+#     #print W.eval()
+#     #sys.exit(0)
+#     session.run(model)
+
     
-    for ii in range(1500):
-        print ".",
-        sys.stdout.flush()
-        ttt = session.run(train_step) # , feed_dict={xx: line, y_: label})
-        if (ii%10 == 9):
-            print "\nEvaluating..."
-            values= session.run([W,acc_t, y_t, label_test, line_test]) # , feed_dict={xx: line_test.eval(), y_: label_test.eval() })
-            #print str(values[0])
-            print "output:" +str(values[2])
-            print "labels: "+str(values[3])
-            print "accuracy: "+ str(values[1])
-            print str(ii)
+#     for ii in range(1500):
+#         print ".",
+#         sys.stdout.flush()
+#         ttt = session.run(train_step) # , feed_dict={xx: line, y_: label})
+#         if (ii%10 == 9):
+#             print "\nEvaluating..."
+#             values= session.run([W,acc_t, y_t, label_test, line_test]) # , feed_dict={xx: line_test.eval(), y_: label_test.eval() })
+#             #print str(values[0])
+#             print "output:" +str(values[2])
+#             print "labels: "+str(values[3])
+#             print "accuracy: "+ str(values[1])
+#             print str(ii)
         
 
-    coord.request_stop()
-    coord.join(threads)
+#     coord.request_stop()
+#     coord.join(threads)
 
-    save_path = saver.save(session, "/tmp/model.ckpt")
-    print("Model saved in file: %s" % save_path)
+#     save_path = saver.save(session, "/tmp/model.ckpt")
+#     print("Model saved in file: %s" % save_path)
